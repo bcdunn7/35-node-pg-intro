86cede467932659aae5abc4e980e40796592a618
Bryce <bryce.cc.dunn@gmail.com>
2021-08-18 14:26:05 -0500

    fixed issue with psql password

--- db.js
+++ db.js
@@ -1,11 +1,18 @@
 const { Client } = require('pg');
 
+// to access .env variables
+const dotenv = require('dotenv');
+dotenv.config();
+
 let DB_URI;
+
+// to deal with invalid characters in password
+const URI_DB_PASSWORD = `${encodeURIComponent(process.env.DB_PASSWORD)}`
 
 if (process.env.NODE_ENV === "test") {
     DB_URI = "postgresql:///biztime_test";
 } else {
-    DB_URI = "postgresql:///biztime";
+    DB_URI = `postgresql://${process.env.DB_USER}:${URI_DB_PASSWORD}@localhost:${process.env.DB_PORT}/biztime`;
 }
 
 let db = new Client({
